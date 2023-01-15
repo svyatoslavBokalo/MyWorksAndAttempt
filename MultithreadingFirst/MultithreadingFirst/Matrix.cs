431636f04661df26fd2885bbc1c70e3266450a8c
svyatoslavBokalo <105822277+svyatoslavBokalo@users.noreply.github.com>
2023-01-15 14:48:33 +0200

    add my first sttempt with threading

---
+++ MultithreadingFirst/MultithreadingFirst/Matrix.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MultithreadingFirst
+{
+    class Matrix
+    {
+        private int rowCount;
+        private int columnCount;
+        private double[,] matr;
+        public double sum;
+        public int startRow;
+        public int endRow;
+        public int startColumn;
+        public int endColumn;
+
+        public int RowCount { get => rowCount; set => rowCount = value; }
+        public int ColumnCount { get => columnCount; set => columnCount = value; }
+        public double[,] Matr { get => matr; set => matr = value; }
+        public Matrix()
+        {
+            this.rowCount = 0;
+            this.columnCount = 0;
+            this.matr = new double[0,0];
+        }
+        public Matrix(int rowCount = 1, int columnCount = 1)
+        {
+            this.rowCount = rowCount;
+            this.columnCount = columnCount;
+            this.matr = new double[rowCount, columnCount];
+        }
+
+
+
+        public Matrix(double[,] a)
+        {
+            this.rowCount = a.GetLength(0);
+            this.columnCount = a.GetLength(1);
+            this.matr = new double[a.GetLength(0), a.GetLength(1)];
+            for(int i = 0; i< a.GetLength(0); i++)
+            {
+                for(int j = 0; j < a.GetLength(1); j++)
+                {
+                    this.matr[i, j] = a[i, j];
+                }
+            }
+        }
+
+        public Matrix(double[] a)
+        {
+            this.rowCount = a.Length;
+            this.columnCount = 1;
+            this.matr = new double[a.GetLength(0), 1];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                this.matr[i, 0] = a[i];
+            }
+        }
+
+        public Matrix(StreamReader sr)
+        {
+            ReadFromFile(sr);
+        }
+
+        public Matrix(Matrix matrix)
+        {
+            this.rowCount = matrix.rowCount;
+            this.columnCount = matrix.columnCount;
+            this.Clone(matrix);
+        }
+
+        public Matrix(int rowCount, int columCount, double[,] matr)
+        {
+            this.rowCount = rowCount;
+            this.columnCount = columCount;
+            this.matr = matr;
+        }
+
+        public Matrix(double[,] matr, int startRow,int endRow, int startColumn, int endColumn)
+        {
+            this.rowCount = matr.GetLength(0);
+            this.columnCount = matr.GetLength(1);
+            this.matr = matr;
+            this.startRow = startRow;
+            this.startColumn = startColumn;
+            this.endRow = endRow;
+            this.endColumn = endColumn;
+        }
+
+
+        public Matrix DirectMove(Matrix left)
+        {
+            Matrix res = new Matrix(left);
+            double cof = 0;
+            double v = 0;
+
+            for (int i = 0; i < left.ColumnCount; i++)
+            {
+                double max = Math.Abs(res[i, i]);
+                int numberMax = i;
+
+                for (int k = i; k < left.RowCount; k++)
+                {
+                    if (Math.Abs(res[k, i]) > max)
+                    {
+                        max = Math.Abs(res[k, i]);
+                        numberMax = k;
+                    }
+                }
+
+                if (i != numberMax)
+                {
+                    for (int j = i; j < left.ColumnCount; j++)
+                    {
+                        double buff = res[i, j];
+                        res[i, j] = res[numberMax, j];
+                        res[numberMax, j] = buff;
+                    }
+                }
+
+                v = res[i, i];
+
+                for (int j = i + 1; j < left.RowCount; j++)
+                {
+                    cof = res[j, i];
+                    for (int k = i; k < left.ColumnCount; k++)
+                    {
+                        res[j, k] = res[j, k] * v - res[i, k] * cof;
+                    }
+                    //right[j, 0] = right[j, 0] * v - right[i, 0] * cof;
+                }
+            }
+
+            return res;
+
+        }
+
+
+
+        public Matrix ReversMove(Matrix left, Matrix right)
+        {
+            Matrix x = new Matrix(right.RowCount, 1);
+
+            for (int i = x.RowCount - 1; i > -1; i--)
+            {
+                double s = 0;
+                for (int k = i + 1; k < left.ColumnCount; k++)
+                {
+                    s += left[i, k] * x[k, 0];
+                }
+                x[i, 0] = (right[i, 0] - s) / left[i, i];
+            }
+
+            return x;
+        }
+
+
+        
+
+        public void ReadFromFile(StreamReader sr)
+        {
+            string line = sr.ReadLine();
+            rowCount = int.Parse(line.Split()[0]);
+            columnCount = int.Parse(line.Split()[1]);
+            matr = new double[rowCount, columnCount];
+
+            for(int i = 0; i < rowCount; i++)
+            {
+                line = sr.ReadLine();
+                string[] mas = line.Split();
+                for(int j = 0; j < columnCount; j++)
+                {
+                    matr[i, j] = double.Parse(mas[j]);
+                }
+            }
+        }
+
+        public void FillingTheMatrix()
+        {
+            for(int i = 0; i < rowCount; i++)
+            {
+                string line = Console.ReadLine();
+                string[] mas = line.Split();
+                for(int j = 0; j < columnCount; j++)
+                {
+                    double number = double.Parse(mas[j]);
+                    matr[i, j] = number;
+                }
+            }
+               
+        }
+
+        static public Matrix operator+(Matrix m1, Matrix m2)
+        {
+            Matrix res = new Matrix(m1.RowCount, m1.ColumnCount);
+
+            if (m1.rowCount == m2.rowCount && m1.rowCount == m2.rowCount)
+            {
+                for (int i = 0; i < m1.rowCount; i++)
+                {
+                    for (int j = 0; j < m1.columnCount; j++)
+                    {
+                        res.matr[i, j] = m1.matr[i,j] + m2.Matr[i, j];
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("incorrect size of matrix");
+            }
+            return res;
+        }
+
+        static public Matrix operator -(Matrix m1, Matrix m2)
+        {
+            Matrix res = new Matrix(m1.RowCount, m1.ColumnCount);
+
+            if (m1.rowCount == m2.rowCount && m1.rowCount == m2.rowCount)
+            {
+                for (int i = 0; i < m1.rowCount; i++)
+                {
+                    for (int j = 0; j < m1.columnCount; j++)
+                    {
+                        res.matr[i, j] = m1.matr[i,j] - m2.matr[i, j];
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("incorrect size of matrix");
+            }
+            return res;
+        }
+
+        static public Matrix operator *(Matrix m1, Matrix m2)
+        {
+            Matrix res = new Matrix(m1.RowCount, m2.ColumnCount);
+            
+            if (m1.ColumnCount == m2.RowCount)
+            {
+                for(int i = 0; i < m1.RowCount; i++)
+                {
+                    for(int j = 0; j < m2.ColumnCount; j++)
+                    {
+                        double sum = 0;
+                        for(int k = 0; k < m1.ColumnCount; k++)
+                        {
+                            sum += m1.Matr[i, k] * m2.Matr[k, j];
+                        }
+                        res.Matr[i, j] = sum;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("incorrect size of matrix");
+            }
+            return res;
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                if((i>rowCount || i < 0) || (j > columnCount || j < 0))
+                {
+                    throw new Exception("incorrect index");
+                }
+                return matr[i, j];
+            }
+            set
+            {
+                if ((i > rowCount || i < 0) || (j > columnCount || j < 0))
+                {
+                    throw new Exception("incorrect index");
+                }
+                this.matr[i, j] = value;
+            }
+        }
+
+        public double Norma()
+        {
+            double res = 0;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    res += matr[i, j] * matr[i, j];
+                }
+            }
+
+            return Math.Sqrt(res);
+        }
+
+        public double Norma1()
+        {
+            double res = Math.Abs(matr[0,0]);
+            for(int i = 0; i < rowCount; i++)
+            {
+                for(int j = 0; j < columnCount; j++)
+                {
+                    if (Math.Abs(matr[i, j]) > res)
+                    {
+                        res = Math.Abs(matr[i, j]);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public void Clone(Matrix matrix)
+        {
+            this.matr = new double[matrix.rowCount, matrix.columnCount];
+            for (int i = 0; i < matrix.rowCount; i++)
+            {
+                for (int j = 0; j < matrix.columnCount; j++)
+                {
+                    matr[i, j] = matrix[i, j];
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    res += string.Format("{0:f6}", matr[i, j]) + " ";
+                }
+                res += "\n";
+            }
+            return res;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    Console.Write(string.Format("{0,5:0.0}", matr[i, j]) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //public void 
+    }
+}
---
+++ MultithreadingFirst/MultithreadingFirst/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.IO;
+
+namespace MultithreadingFirst
+{
+    public class MyData
+    {
+        public int a, b;
+        public int[] mas;
+        public int startIndex;
+        public int count;
+        public int sum;
+
+        public MyData(int[] mas, int startIndex, int count)
+        {
+            this.mas = mas;
+            this.startIndex = startIndex;
+            this.count = count;
+            this.sum = 0;
+        }
+    }
+
+    public class Parametr
+    {
+        public double[,] matr;
+        public double sum;
+        public int startRow;
+        public int endRow;
+        public int startColumn;
+        public int endColumn;
+
+        public Parametr(double[,] matr, int startRow, int endRow, int startColumn, int endColumn)
+        {
+            this.matr = matr;
+            this.startRow = startRow;
+            this.startColumn = startColumn;
+            this.endRow = endRow;
+            this.endColumn = endColumn;
+        }
+    }
+
+    public class ThreadExample
+    {
+        public int sum;
+        public ThreadExample()
+        {
+            this.sum = 0;
+        }
+        // The ThreadProc method is called when the thread starts.
+        // It loops ten times, writing to the console and yielding
+        // the rest of its time slice each time, and then ends.
+        public static void ThreadProc(object data)
+        {
+            //MyData data1 = (MyData)data;
+            //for (int i = data1.startIndex; i < data1.startIndex+data1.count; i++)
+            //{
+            //    data1.sum += data1.mas[i];
+            //    //Thread.Sleep(2000);
+            //}
+            //data = data1;
+
+            Matrix data1 = (Matrix)data;
+            for(int i = data1.startRow; i < data1.endRow; i++)
+            {
+                for(int j = data1.startColumn; j < data1.endColumn; j++)
+                {
+                    data1.sum += data1[i, j];
+                }
+            }
+            data = data1;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matr = new double[12, 12];
+            Random rnd = new Random();
+            for(int i = 0; i < 12; i++)
+            {
+                for(int j = 0; j < 12; j++)
+                {
+                    matr[i, j] = rnd.Next(-10, 10);
+                }
+            }
+
+            Matrix matrix = new Matrix(matr);
+            matrix.Show();
+            double sum1 = 0;
+            for (int i = 0; i < matrix.RowCount/2; i++)
+            {
+                for(int j = 0; j < matrix.ColumnCount/2; j++)
+                {
+                    sum1 += matrix[i, j];
+                }
+                
+            }
+
+            double sum2 = 0;
+            for (int i = 0; i < matrix.RowCount/2; i++)
+            {
+                for (int j = matrix.ColumnCount / 2; j < matrix.ColumnCount; j++)
+                {
+                    sum2 += matrix[i, j];
+                }
+
+            }
+
+            double sum3 = 0;
+            for (int i = matrix.RowCount / 2; i < matrix.RowCount; i++)
+            {
+                for (int j = 0; j < matrix.ColumnCount / 2; j++)
+                {
+                    sum3 += matrix[i, j];
+                }
+
+            }
+
+            double sum4 = 0;
+            for (int i = matrix.RowCount / 2; i < matrix.RowCount; i++)
+            {
+                for (int j = matrix.ColumnCount / 2; j < matrix.ColumnCount; j++)
+                {
+                    sum4 += matrix[i, j];
+                }
+
+            }
+            Console.WriteLine($"sum1 = {sum1} \t sum2 = {sum2} \t sum3 = {sum3} \t sum4 = {sum4} \n sum = {sum1 + sum2 + sum3 + sum4}");
+
+            int n = 6;
+            int m = 6;
+            int countThread = 2;
+            int countElementForRow = matrix.RowCount/ (n);
+            int countElementForColumn = matrix.ColumnCount / (m);
+
+            
+            Matrix[] myMatrix = new Matrix[n*m];
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    myMatrix[k] = new Matrix(matr, i*countElementForRow, i * countElementForRow+countElementForRow,
+                        j*countElementForColumn, j * countElementForColumn + countElementForColumn);
+                    k++;
+                }
+            }
+
+            Thread[] threads = new Thread[countThread];
+            for (int i = 0; i < n*m/countThread; i++)
+            {
+                for(int j = 0; j < countThread; j++)
+                {
+                    threads[j] = new Thread(new ParameterizedThreadStart(ThreadExample.ThreadProc));
+                    threads[j].Start(myMatrix[i*countThread+j]);
+                    threads[j].Join();
+                }
+            }
+
+            double allSum = 0;
+            foreach(Matrix el in myMatrix)
+            {
+                allSum += el.sum;
+                Console.WriteLine("sum = " + el.sum);
+            }
+            Console.WriteLine("all sum = " + allSum.ToString());
+
+
+            
+
+
+
+
+            //foreach(MyData data in myDatas)
+            //{
+            //    Console.WriteLine("sum = " + data.sum);
+            //}
+            //int[] mas = new int[10];
+            //Random rnd = new Random();
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    mas[i] = rnd.Next(-100, 100);
+            //}
+            //int sum1 = 0;
+            //for (int i = 0; i < mas.Length / 2; i++)
+            //{
+            //    sum1 += mas[i];
+            //}
+            //int sum2 = 0;
+            //for (int i = mas.Length / 2; i < mas.Length; i++)
+            //{
+            //    sum2 += mas[i];
+            //}
+            //Console.WriteLine($"sum1 = {sum1} \t sum2 = {sum2} \t\t sum = {sum1 + sum2}");
+
+            //int countThread = 2;
+            //int countElement = mas.Length / countThread;
+            //Thread[] threads = new Thread[countThread];
+            //MyData[] myDatas = new MyData[countThread];
+            //for(int i = 0; i<countThread; i++)
+            //{
+            //    threads[i] = new Thread(new ParameterizedThreadStart(ThreadExample.ThreadProc));
+            //    myDatas[i] = new MyData(mas, i * countElement, countElement);
+            //    threads[i].Start(myDatas[i]);
+            //    threads[i].Join();
+            //}
+
+            //foreach(MyData data in myDatas)
+            //{
+            //    Console.WriteLine("sum = " + data.sum);
+            //}
+        }
+    }
+}
